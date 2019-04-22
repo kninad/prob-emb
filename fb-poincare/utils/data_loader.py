103b8db5520357f6ef4bf5b0b848f019ed258f04
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2019-04-22 17:16:40 -0400

    added csv code

--- fb-poincare/utils/data_loader.py
+++ fb-poincare/utils/data_loader.py
@@ -30,12 +30,40 @@ def get_data_list(data_filepath, prob_threshold):
     # drop the 3rd column now., since no use
     df.drop('cond_prob', axis=1, inplace=True)
     data_list = list(df.itertuples(index=False, name=None))
-    outFile = data_filepath + "_HBoutput.txt"
+
+    outFile = data_filepath + "_HB_gensim.csv"
     with open(outFile, "w") as out:
+        out.write("id1,id2,weight\n")
         for row in data_list:
             out.write("%s,%s,1\n" % (row[0], row[1]))
     return data_list
     
 
+def get_mod_data(data_filepath, prob_threshold):
+    """Function to create a list of training tuples. It is according
+    to the training data format specified for the poincare model.
+    
+    Parameters
+    ----------
+    data_filepath : str
+        csv file Path having the conditional probabilities. format is
+        like - IsA \t term1 \t term2 \t prob.    
+    prob_threshold : float
+        threshold for the conditional probability. Only pairs having 
+        prob greater than this will be considered.
+
+    Returns
+    -------
+    data_list : list
+        List of training tuples (pairs) having 2 terms which satisfy 
+        the threshold requirement.             
+    """
+    # data_list = []    
+    df = pd.read_csv(data_filepath, header=None, delimiter='\t', usecols=[1,2,3])
+    df.columns = ['id1', 'id2', 'weight']
+    outFile = data_filepath + "_hb.csv"
+    df.to_csv(outFile, sep=',', index=False)
+    
+    return None
 
 
