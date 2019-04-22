d7aa7687e2b68c6013724ec0ad297ca0ff9a6659
Ninad Khargonkar <ninad.khargonkar@gmail.com>
2019-04-22 14:57:24 -0400

    cmd line arg chang

--- poincare/experiments/trainer.py
+++ poincare/experiments/trainer.py
@@ -29,7 +29,7 @@ def train_run(args):
     exp_name = 'HB'+'time' + str(datetime.now()) + '_EXP' + str(args.train_dir) + \
     '_prbt' + str(args.prob_threshold) + '_reg' + str(args.reg_coef) + \
     '_dim' + str(args.embed_dim) + '_lr' + str(args.learning_rate) + \
-    '_neg' + str(args.neg_samples) + '_epoc' + str(args.epochs) + '_burnin' + str(args.burn_in)
+    '_neg' + str(args.negs) + '_epoc' + str(args.epochs) + '_burnin' + str(args.burn_in)
 
     exp_name = exp_name.replace(":", "-")
     exp_name = exp_name.replace("/", "-")
@@ -51,10 +51,11 @@ def train_run(args):
     
     training_file = args.train_dir + args.trn_file
     trn_dataset = data_loader.get_data_list(training_file, args.prob_threshold)
+    print("Number of training examples: ", len(trn_dataset))
     
     # Create the model definition
     model = PoincareModel(train_data=trn_dataset, size=args.embed_dim, alpha=args.learning_rate,
-                    negative=args.neg_samples, regularization_coeff=args.reg_coef, 
+                    negative=args.negs, regularization_coeff=args.reg_coef, 
                     burn_in=args.burn_in, burn_in_alpha=args.burn_in_alpha, 
                     init_range=args.init_range, seed=args.random_seed)
 
@@ -81,7 +82,7 @@ def main():
     parser.add_argument('--params_folder', type=str, default='./params/', help='Params folder location')
     
     # Dataset 
-    parser.add_argument('--train_dir', type=str, default='./data/book_data/book_small/', help='Directory for data files')
+    parser.add_argument('--train_dir', type=str, default='./data/book_data/exp2.3_baseline_notaxo/', help='Directory for data files')
     
     parser.add_argument('--trn_file', type=str, default='book_train.txt', help='Training file')
     parser.add_argument('--trn_eval', type=str, default='book_train_eval.txt', help='Trn eval file')
@@ -91,10 +92,10 @@ def main():
 
     parser.add_argument('--prob_threshold', type=float, default=0.5, help='Conditional probability threshold')
     
-    # Model defn
+    # Model define params
     parser.add_argument('--embed_dim', type=int, default=50, help='Number of dimensions of the trained model')
     parser.add_argument('--learning_rate', type=float, default=0.1, help='Learning rate for training (alpha)')
-    parser.add_argument('--neg_samples', type=int, default=10, help='Number of negative samples to use')
+    parser.add_argument('--negs', type=int, default=10, help='Number of negative samples to use')
     parser.add_argument('--reg_coef', type=float, default=1.0, help='Coefficient used for l2-regularization while training')
     parser.add_argument('--burn_in', type=int, default=10, help='Number of epochs to use for burn-in initialization')
     parser.add_argument('--burn_in_alpha', type=float, default=0.01, help='Learning rate for burn-in initialization')
@@ -102,8 +103,8 @@ def main():
     
     # Model trainig
     parser.add_argument('--epochs', type=int, default=1, help='Number of iterations (epochs) over the corpus')
-    parser.add_argument('--batch_size', type=int, default=10, help='Number of iterations (epochs) over the corpus')
-    parser.add_argument('--print_every', type=int, default=100, help='Prints progress and average loss after every `print_every` batches')
+    parser.add_argument('--batch_size', type=int, default=20, help='Number of examples to train on in a single batch')
+    parser.add_argument('--print_every', type=int, default=500, help='Prints progress and average loss after every `print_every` batches')
     
     # Parse the arguments                              
     args = parser.parse_args()
